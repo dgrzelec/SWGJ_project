0e38701792ab14d35200bcb0a9b83ae3c1d484dc
Daniel Grzelec <danielgrzelec@gmail.com>
2023-02-24 22:36:29 +0100

    basic weapon implementation

--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -2,11 +2,37 @@ extends Node
 
 class_name Weapon
 
-onready var current_bullet = $Bullet
+var current_bullet:PackedScene  = null
 onready var sprite = $Sprite
+onready var shoot_pos = $ShootPosition
+onready var animation_player = $AnimationPlayer
+
+var shoot_dir = Vector2.ZERO #has to be updated from players side
+
 
 
 func _ready():
 	pass # Replace with function body.
 
 
+func set_bullet_type(bullet: PackedScene):
+	if bullet == null:
+		print("null bullet type")
+		return
+		
+	current_bullet = bullet
+	
+func shoot():
+	var bullet_instance = current_bullet.instance()
+	bullet_instance.position = shoot_pos.global_position
+	bullet_instance.direction = shoot_dir
+	add_child(bullet_instance)
+	
+func shoot_bullet(bullet: PackedScene):
+	var bullet_instance = bullet.instance()
+	bullet_instance.position = shoot_pos.global_position
+	bullet_instance.direction = shoot_dir
+	add_child(bullet_instance)
+
+func set_shoot_dir(dir: Vector2):
+	shoot_dir = dir
