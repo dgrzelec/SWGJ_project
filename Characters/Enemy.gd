48e85db6e4ba38455a9469e5e1d28b9f619fa0af
WojciechSokolowski <soko19@op.pl>
2023-02-25 12:31:48 +0100

    end mission screen and kill counter

--- Characters/Enemy.gd
+++ Characters/Enemy.gd
@@ -2,7 +2,13 @@ extends KinematicBody2D
 
 class_name Enemy
 
+signal EnemyKill
+
 export var health_points = 50
 
 func take_damage(damage):
 	print("OWO master, punish me more, I took damage ", damage)
+
+func EnemyKilled():
+	emit_signal("EnemyKill")
+	queue_free()
